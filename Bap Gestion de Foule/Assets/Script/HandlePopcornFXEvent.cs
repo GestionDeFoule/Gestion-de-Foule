2791e799842c6d8b8584afe86397a8cbc0f0b473
Unknown <cedric.chauvin99@gmail.com>
2019-03-22 10:37:51 +0100

    IA

---
+++ Bap Gestion de Foule/Assets/Script/HandlePopcornFXEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandlePopcornFXEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PKFxEventManager.RegisterCustomHandler(OnRaiseEvent);
+
+    }
+
+    private void OnRaiseEvent(string eventName, Vector3 position)
+    {
+        Debug.Log(eventName);
+    }
+}
