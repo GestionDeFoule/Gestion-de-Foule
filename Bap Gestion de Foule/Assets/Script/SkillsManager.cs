a52df298387364f04f7a4016c66e05af283bfafb
Unknown <cedric.chauvin99@gmail.com>
2019-06-09 20:55:37 +0200

    modif camera+Skill

--- Bap Gestion de Foule/Assets/Script/SkillsManager.cs
+++ Bap Gestion de Foule/Assets/Script/SkillsManager.cs
@@ -5,9 +5,45 @@ using UnityEngine;
 public class SkillsManager : MonoBehaviour {
 
     public GameObject prefabSkill;
+    GameObject prefab;
+    bool firstClick;
+    public float ScaleFactor = 50f;
+    public float PosY;
+    private Vector3 position;
+    [Range(0.0f, 60.0f)]
+    public float smooth;
 
     public void Skill1()
     {
-        GameObject prefab = Instantiate(prefabSkill);
+        if (prefab != null)
+            Destroy(prefab);
+        prefab = Instantiate(prefabSkill,position,prefabSkill.transform.rotation);
+        prefab.transform.localScale = new Vector3(prefab.transform.localScale.x * ScaleFactor, prefab.transform.localScale.y * ScaleFactor, prefab.transform.localScale.z);
+        firstClick = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            if (prefab != null)
+            {
+                if (firstClick)
+                    firstClick = false;
+                else
+                    Destroy(prefab);
+
+            }
+    }
+
+
+    void FixedUpdate()
+    {
+        Vector3 mp = Input.mousePosition;
+        mp.z = Camera.main.transform.position.y;
+        //mp.z = Camera.main.transform.position.magnitude;
+        position = Camera.main.ScreenToWorldPoint(mp);
+        position.y = PosY;
+        if(prefab!=null)
+            prefab.transform.position = Vector3.Lerp(prefab.transform.position, position, smooth * Time.fixedDeltaTime);
     }
 }
